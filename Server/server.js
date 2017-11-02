2bc998e18e8f083a3472f7329d3b0b137ac72d05
Your Name <sheel.bedi@gmail.com>
2017-11-01 21:01:42 -0500

    add css and REST routes

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+module.exports = app;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('client'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname,  '../client/', 'index.html'));
+});
+
+// GET method route
+app.get('/sampleRoute', function (req, res) {
+  res.send('GET request to the homepage')
 })
+
+// POST method route
+app.post('/sampleRoute', function (req, res) {
+  res.send('POST request to the homepage')
+})
+
+var port = process.env.PORT || 4040;
+app.listen(port);
+console.log("Listening on port " + port);
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
